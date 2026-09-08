195566b5f5fb477308572a02c978bbff6614f93c
Ayesharif <ayeshrf@gmail.com>
2026-09-08 14:31:10 +0500

    Complete Backend

--- server.js
+++ server.js
@@ -80,8 +80,7 @@ const app = express();
 // Middleware
 const allowedOrigins = [
   process.env.CLIENT_URL || 'http://localhost:5173',
-  'http://localhost:3000',
-  'http://127.0.0.1:5173'
+  'https://muhammadayesh.vercel.app'
 ];
 
 app.use(
